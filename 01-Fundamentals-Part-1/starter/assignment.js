5e945b4787e543107abedc8c020f1026f82887b2
billyusal <79430518+billyusal@users.noreply.github.com>
2023-03-02 09:59:14 +0700

    assignment and script

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,8 @@
+let country = 'Indonesia', continent = 'asia', population = 1
+
+console.log(country, continent, population);
+
+let isIsland = true;
+let language = indonesian;
+
+console.log(isIsland, country, continent, population, language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,50 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+
+// let javascriptIsFun = true;
+// console.log(true);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'yes!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
+
